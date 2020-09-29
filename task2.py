a7c613c0a6dfbfd0911ce0d48fa30befc9250b9b
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-09-29 13:02:52 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,3 +1,4 @@
+  
 #! python3
 # Task2.py
 
@@ -5,15 +6,12 @@
 Ask the user to enter a number.
 Tell them if the number is both a perfect square and a perfect cube.
 (2 points) 
-
 Inputs:
 number
-
 Outputs:
 xx is both a perfect square and a perfect cube.
 xx is only a perfect square.
 xx is only a perfect cube.
-
 Example:
 Enter a number: 8
 8 is only a perfect cube.
@@ -21,11 +19,11 @@ Enter a number: 8
 import math
 
 number=input("Enter a number")
-number=float(number)
+x=float(number)
 
-if number**float(1.0/3)%1==0 and math.sqrt(number)%1==0:
-    print(str(number)+" "+"is both a perfect square and a perfect cube.")
-if number**float(1.0/3)%1==0:
-    print(str(number)+" "+"is only a perfect cube.")
-if math.sqrt(number)%1==0:
-    print(str(number)+" "+"is only a perfect square.")
+if round(x**float(1/3),6)%1==0 and math.sqrt(x)%1==0:
+    print(number+" "+"is both a perfect square and a perfect cube.")
+elif round(x**float(1/3),6)%1==0:
+    print(number+" "+"is only a perfect cube.")
+elif math.sqrt(x)%1==0:
+    print(number+" "+"is only a perfect square.")
